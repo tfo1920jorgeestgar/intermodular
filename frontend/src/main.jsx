b9d03a95abf8953c9194c96ead4efd298b2f04b1
yeray-752 <yeraycarrioncero21@gmail.com>
2025-12-02 21:58:55 +0000

    bien

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from './pages/Home.jsx'
-import Prueba from './pages/prueba.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router'
 import ReactDOM from "react-dom/client"
 
